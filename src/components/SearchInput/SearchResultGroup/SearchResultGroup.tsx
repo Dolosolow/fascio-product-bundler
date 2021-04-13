a627ed68a6001cf56ee9d8f8be34899803bcf960
Dolosolow <Jose.Munoz07c@gmail.com>
2021-04-12 22:13:02 -0400

    Minor bug fix. Search checkbox error thrown when search field is empty but items are selected.

--- src/components/SearchInput/SearchResultGroup/SearchResultGroup.tsx
+++ src/components/SearchInput/SearchResultGroup/SearchResultGroup.tsx
@@ -1,36 +1,20 @@
 import { VStack, CheckboxGroup, Checkbox, Spinner } from '@chakra-ui/react';
 import { useFormikContext } from 'formik';
 import { useState, useEffect } from 'react';
-import { useLazyQuery, gql } from '@apollo/client';
+import { useLazyQuery } from '@apollo/client';
 import _ from 'lodash';
 
 import SearchCheckboxForm from '../SearchCheckboxForm';
 import ResultItem from './ResultItem';
 
 import { Product } from 'src/types/schema';
+import { productsByKeyword } from 'src/graphql/queries';
 
 interface SRGProps {
   searchInput: string;
   multipleSelection?: boolean;
   resetTermField: () => void;
 }
-
-const productsByKeyword = gql`
-  query productsByKeyword($keyword: String!) {
-    productsByKeyword(keyword: $keyword) {
-      id
-      sku
-      name
-      price
-      sale_price
-      primary_image {
-        id
-        description
-        url_thumbnail
-      }
-    }
-  }
-`;
 
 const SearchResultsGroup = ({
   multipleSelection = false,
@@ -75,7 +59,7 @@ const SearchResultsGroup = ({
     if (called && loading)
       return <ResultItem error={<Spinner color="teal.500" thickness="3px" size="xl" />} />;
 
-    if (called && !loading) {
+    if (called && !loading && data) {
       return !data!['productsByKeyword'].length ? (
         <ResultItem error={`No results for "${searchInput}"`} />
       ) : (
@@ -103,7 +87,11 @@ const SearchResultsGroup = ({
       fetchProducts({ variables: { keyword: term } });
     };
 
-    fetchProductsByKeyword();
+    if (searchInput === '') {
+      resetAllFields();
+    } else {
+      fetchProductsByKeyword();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchInput]);
 
@@ -144,123 +132,3 @@ const SearchResultsGroup = ({
 };
 
 export default SearchResultsGroup;
-
-//
-//
-
-// import { VStack, CheckboxGroup, Checkbox } from '@chakra-ui/react';
-// import { useFormikContext } from 'formik';
-// import { useState, useEffect } from 'react';
-// import _ from 'lodash';
-
-// import SearchCheckboxForm from '../SearchCheckboxForm';
-// import ResultItem from './ResultItem';
-
-// import { testTableValues } from 'src/data/TestData';
-
-// interface SRGProps {
-//   searchInput: string;
-//   multipleSelection?: boolean;
-//   resetTermField: () => void;
-// }
-
-// const SearchResultsGroup = ({
-//   multipleSelection = false,
-//   searchInput,
-//   resetTermField,
-// }: SRGProps) => {
-//   const { values, setValues } = useFormikContext<Builder.Grup.BuilderMap>();
-//   const [searchResults, setSearchResults] = useState<any[]>([]);
-//   const [products, setProducts] = useState<any[]>([]);
-//   const [section, setSection] = useState('');
-
-//   const renderResultList = () => (
-//     <VStack maxH="380px" overflowY="scroll" w="100%">
-//       {!searchResults.length ? (
-//         <ResultItem error={`No results for "${searchInput}"`} />
-//       ) : (
-//         searchResults.map((result) => {
-//           const { id, data } = result;
-//           return (
-//             <ResultItem
-//               key={id}
-//               title={data.product_name.value}
-//               img={data.image.value}
-//               price={data.price.value}
-//             >
-//               <Checkbox size="lg" value={id} />
-//             </ResultItem>
-//           );
-//         })
-//       )}
-//     </VStack>
-//   );
-
-//   const addProducts = (newProducts: any) => {
-//     setProducts([...newProducts]);
-//   };
-
-//   const onSectionChange = (section: string) => {
-//     setSection(section);
-//   };
-
-//   const resetAllFields = () => {
-//     resetTermField();
-//     setProducts([]);
-//   };
-
-//   const submitProducts = () => {
-//     const updatedSections = _.chain(values.content.steps).keyBy('instructions').value();
-//     products.forEach((prod) => updatedSections[section].products.push(prod));
-//     setValues({ ...values, content: { steps: _.values(updatedSections) } });
-//   };
-
-//   useEffect(() => {
-//     if (searchInput.length > 0) {
-//       const term = searchInput;
-//       const results = testTableValues.filter((item) =>
-//         item.data['product_name'].value.toLocaleLowerCase().includes(term)
-//       );
-//       setSearchResults(results);
-//     } else {
-//       setSearchResults([]);
-//     }
-//   }, [searchInput]);
-
-//   return (
-//     <VStack
-//       spacing={0}
-//       hidden={!searchInput.length}
-//       borderTop="none"
-//       borderWidth={1}
-//       borderTopLeftRadius={0}
-//       borderTopRightRadius={0}
-//       boxShadow="lg"
-//       rounded="md"
-//       bg="white"
-//       h="max-content"
-//       position="absolute"
-//       top={12}
-//       w="100%"
-//       zIndex={100}
-//     >
-//       <CheckboxGroup
-//         colorScheme="teal"
-//         onChange={(values) => addProducts(values)}
-//         value={searchInput === '' ? [] : products}
-//       >
-//         {renderResultList()}
-//       </CheckboxGroup>
-//       {multipleSelection && (
-//         <SearchCheckboxForm
-//           totalSelectedProducts={products.length}
-//           onSectionChange={onSectionChange}
-//           onSubmitProducts={submitProducts}
-//           resetTermField={resetAllFields}
-//         />
-//       )}
-//     </VStack>
-//   );
-// };
-
-// export default SearchResultsGroup;
---
+++ src/graphql/queries.ts
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+export const productsByKeyword = gql`
+  query productsByKeyword($keyword: String!) {
+    productsByKeyword(keyword: $keyword) {
+      id
+      sku
+      name
+      price
+      sale_price
+      primary_image {
+        id
+        description
+        url_thumbnail
+      }
+    }
+  }
+`;
